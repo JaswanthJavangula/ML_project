bce9c5bc575dbc97a03147726e51ea9f3dc621c1
jaswanthjavangula <javangulajaswanth@gmail.com>
2026-07-30 14:19:07 -0500

    EDA

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 Log_file = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs", Log_file)
+logs_path = os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path, exist_ok = True)
 
 Log_file_path = os.path.join(logs_path, Log_file)
@@ -13,3 +13,4 @@ logging.basicConfig(
     format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO
 )
+
