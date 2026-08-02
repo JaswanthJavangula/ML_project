8dc8a243715a0028966bdf63a324d93a6ab2da70
jaswanthjavangula <javangulajaswanth@gmail.com>
2026-08-02 16:52:23 -0500

    Deployement AWS Configuration Updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
 
 from src.exception import CustomException
 from src.logger import logging
@@ -16,7 +16,7 @@ from src.utils import save_object
 from src.utils import evaluate_models
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("aritifacts", "model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
 class ModelTrainer:
     def __init__(self):
@@ -36,9 +36,9 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
-                "XGB Regressor": XGBRegressor()
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
             }
-            
+
             params = {
                 "Decision Tree": {
                      "splitter":['best', 'random'],
@@ -63,10 +63,6 @@ class ModelTrainer:
                     'n_neighbors': [5, 7, 9, 11],
                     'weights': ['uniform', 'distance'],
                     'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
-                },
-                "AdaBoost Regressor": {
-                    'learning_rate': [.1, .01, 0.5, .001],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
                 "CatBoost Regressor": {
                     'depth': [6, 8, 10],
